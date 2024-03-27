c117328b1502181ce5caa976f7484283a4d21407
malicelebii <mehmetalicelebii@gmail.com>
2024-03-27 11:24:32 +0300

    Initial Commit

---
+++ News-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News-App
+//
+//  Created by Mehmet Ali ÇELEBİ on 27.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
